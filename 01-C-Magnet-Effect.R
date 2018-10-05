570ecc93c2c5bdf65269d1518ca09e3d1fe676c5
Einzbern <ylwpaopao@163.com>
2018-10-05 15:18:27 +0800

    Adapt to multi-thresholds.

--- 01-C-Magnet-Effect.R
+++ 01-C-Magnet-Effect.R
@@ -2,41 +2,61 @@ library(tidyverse)
 library(lubridate)
 library(zoo)
 library(parallel)
-options(mc.cores = 4L)
 library(cnquant)
-load("~/Documents/Stock-Data/D-Stock-Daily-Data.RData")
 
-# 初筛样本，需要的变量和样本期，initialize ----
-start_date <- "20141009"  # Tick数据开始的时间
+# Initialize ----
+# 到基础股票数据的路径
+path_to_basic <- "~/Documents/Stock-Data"
+# 读取基础股票数据
+load(paste0(path_to_basic, "/D-Stock-Daily-Data.RData"))
+
+# 最终样本期间为对应有Tick数据的时间
+start_date <- "20141010"  # Tick数据开始的时间
 end_date <- "20180330"  # Tick数据结束的时间
-Stock_Daily_Data0 <- Stock_Daily_Data %>% 
-  # 需要的变量
-  select(S_INFO_WINDCODE:S_DQ_AMOUNT, S_DQ_TRADESTATUS, UP_DOWN_LIMIT_STATUS, ST) %>% 
-  # 样本期
-  filter(TRADE_DT >= start_date) %>% 
-  filter(TRADE_DT <= end_date) %>% 
-  # ! 目前只用主板数据
-  filter(substr(S_INFO_WINDCODE, 1L, 3L) == "000" | substr(S_INFO_WINDCODE, 1L, 1L) == "6") %>% 
-  # 非ST
-  filter(!ST) %>% 
-  # 正常交易状态
-  filter(S_DQ_TRADESTATUS %in% c("交易", "DR", "XD", "XR")) %>% 
-  # 去掉最高价超过涨停价的有问题的样本点，20141009之后只有一个20151218复牌的000520，2块到20块
-  filter(S_DQ_HIGH <= S_DQ_PRECLOSE * 1.1)
 
-Stock_Daily_Data <- Stock_Daily_Data %>% 
-  # 需要的变量
-  select(S_INFO_WINDCODE:S_DQ_AMOUNT, S_DQ_TRADESTATUS, UP_DOWN_LIMIT_STATUS, ST) %>% 
-  # 样本期，因为需要滚动窗口，这里比Tick样本期间多一部分
-  filter(TRADE_DT >= "20140309") %>% 
-  # ! 目前只用主板数据
-  filter(substr(S_INFO_WINDCODE, 1L, 3L) == "000" | substr(S_INFO_WINDCODE, 1L, 1L) == "6")
+# 并行运算使用核心数量
+ncl <- 3L
+socket_type <- "PSOCK"
 
-# 到红色5T含有主板数据移动硬盘下A股数据的路径
-path_to_red_ashare <- "/mnt/sdc2/WIND_DATA/ID_BT_SHARES_A"
+# 使用哪些板的股票
+board_codes <- c("000", "001", "600", "601", "603")
+# 注意现在只使用了主板数据
+# 到主板数据的路径
+path_to_mainboard <- "/mnt/sdc2/WIND_DATA/ID_BT_SHARES_A"
 
 # 涨跌幅阈值
 threshold <- 0.09
+thresholds <- seq(0.09, 0.099, by = 0.001)
+
+
+# # 留存一份初始数据（标号为0#的） ----
+# # 注意这里直接筛选到了最终样本期，而另一部分数据因为需要滚动计算开始时比样本期更长
+# Stock_Daily_Data0 <- Stock_Daily_Data %>% 
+#   # 需要的变量
+#   select(S_INFO_WINDCODE:S_DQ_CLOSE, S_DQ_PCTCHANGE, S_DQ_TRADESTATUS, UP_DOWN_LIMIT_STATUS, ST) %>% 
+#   # 样本期
+#   filter(TRADE_DT >= start_date) %>% 
+#   filter(TRADE_DT <= end_date) %>% 
+#   # 使用哪些板的股票
+#   filter(substr(S_INFO_WINDCODE, 1L, 3L) %in% board_codes)
+# 
+# Stock_Daily_Data01 <- Stock_Daily_Data0 %>% 
+#   # 非ST
+#   filter(!ST) %>% 
+#   # 正常交易状态
+#   filter(S_DQ_TRADESTATUS %in% c("交易", "DR", "XD", "XR")) %>% 
+#   # 去掉最高价超过涨停价的有问题的样本点，20141009之后只有一个20151218复牌的000520，2块到20块
+#   filter(S_DQ_HIGH <= round(S_DQ_PRECLOSE * 1.1 + .Machine$double.eps ^ 0.5, 2))
+
+
+# 初筛样本，需要的变量和时间区间 ----
+Stock_Daily_Data <- Stock_Daily_Data %>% 
+  # 需要的变量
+  select(S_INFO_WINDCODE:S_DQ_CLOSE, S_DQ_PCTCHANGE, S_DQ_TRADESTATUS, UP_DOWN_LIMIT_STATUS, ST) %>% 
+  # 样本期，因为需要滚动窗口，这里比Tick样本期间多取6个月
+  filter(TRADE_DT >= format(start_date %>% ymd() %m-% months(6), "%Y%m%d")) %>% 
+  # 使用哪些板的股票
+  filter(substr(S_INFO_WINDCODE, 1L, 3L) %in% board_codes)
 
 
 # 生成需要变量 ----
@@ -53,6 +73,7 @@ Stock_Daily_Data <- Stock_Daily_Data %>%
 
 
 # 日度数据滚动估计每日的参数mu和sigma ----
+# 估计完后调整到标准样本期
 # 估计均值和标准差的函数
 mu_sigma <- function(x) c(mean(x, na.rm = TRUE), sd(x, na.rm = TRUE))
 
@@ -77,29 +98,37 @@ Stock_Daily_Data <- Stock_Daily_Data %>%
 )
 
 
-# 二筛样本，最大绝对涨幅超过threshold（如9%）的样本点 ----
-Stock_Daily_Data <- Stock_Daily_Data %>% 
-  # 生成kline文件路径
-  mutate(kline_path = if_else(
-    substr(S_INFO_WINDCODE, 1L, 1L) == 6, 
-    paste0(path_to_red_ashare, "/KLine/SH/", TRADE_DT, "/", substr(S_INFO_WINDCODE, 1L, 6L), ".csv"), 
-    paste0(path_to_red_ashare, "/KLine/SZ/", TRADE_DT, "/", substr(S_INFO_WINDCODE, 1L, 6L), ".csv")
+# 添加前一天的Kline数据路径 ----
+Stock_Daily_Data1 <- Stock_Daily_Data %>% 
+  mutate(kline_path_lag1 = case_when(
+    substr(S_INFO_WINDCODE, 1L, 3L) %in% c("000", "001") ~ 
+      paste0(path_to_mainboard, "/KLine/SZ/", TRADE_DT, "/", substr(S_INFO_WINDCODE, 1L, 6L), ".csv"), 
+    substr(S_INFO_WINDCODE, 1L, 3L) %in% c("600", "601", "603") ~ 
+      paste0(path_to_mainboard, "/KLine/SH/", TRADE_DT, "/", substr(S_INFO_WINDCODE, 1L, 6L), ".csv")
   )) %>% 
   # 类似的也可以用前一天的高频数据估计试试，生成前一天的路径，因为要lag日期，所以放在filter之前
   group_by(S_INFO_WINDCODE) %>% 
-  mutate(kline_path_lag1 = lag(kline_path)) %>% 
-  ungroup() %>% 
-  # 只需保留过阈值的交易日
-  filter(REACH_UP_THRESHOLD | REACH_DOWN_THRESHOLD) %>% 
-  # 对涨跌分别去掉开盘时就超过阈值的
-  filter(!(S_DQ_OPEN >= UP_THRESHOLD & S_DQ_LOW > DOWN_THRESHOLD), 
-         !(S_DQ_OPEN <= DOWN_THRESHOLD & S_DQ_HIGH < UP_THRESHOLD)) %>% 
+  mutate(kline_path_lag1 = lag(kline_path_lag1)) %>% 
+  ungroup()
+
+
+# 二筛样本，非ST等破坏个股连续时间样本的要求 ----
+Stock_Daily_Data1 <- Stock_Daily_Data1 %>% 
   # 非ST
   filter(!ST) %>% 
   # 正常交易状态
   filter(S_DQ_TRADESTATUS %in% c("交易", "DR", "XD", "XR")) %>% 
   # 去掉最高价超过涨停价的有问题的样本点，20141009之后只有一个20151218复牌的000520，2块到20块
   filter(S_DQ_HIGH <= UP_LIMIT)
+
+
+# 三筛样本，最大绝对涨幅超过threshold（如9%）的样本点 ----
+Stock_Daily_Data2 <- Stock_Daily_Data1 %>% 
+  # 只需保留过阈值的交易日
+  filter(REACH_UP_THRESHOLD | REACH_DOWN_THRESHOLD) %>% 
+  # 对涨跌分别去掉开盘时就超过阈值的
+  filter(!(S_DQ_OPEN >= UP_THRESHOLD & S_DQ_LOW > DOWN_THRESHOLD), 
+         !(S_DQ_OPEN <= DOWN_THRESHOLD & S_DQ_HIGH < UP_THRESHOLD))
 
 
 # 日内数据估计每日的参数mu和sigma（an alternative way） ----
@@ -114,14 +143,14 @@ mu_sigma_intraday <- function(path) {
 }
 
 system.time(
-Stock_Daily_Data <- Stock_Daily_Data %>% 
+Stock_Daily_Data2 <- Stock_Daily_Data2 %>% 
   # 计算
   data.frame(
-    t(sapply(.$kline_path, mu_sigma_intraday, USE.NAMES = FALSE)), 
+    t(sapply(.$kline_path_lag1, mu_sigma_intraday, USE.NAMES = FALSE)), 
     stringsAsFactors = FALSE
   ) %>% 
   as_tibble() %>% 
-  select(-kline_path, -kline_path_lag1) %>% 
+  select(-kline_path_lag1) %>% 
   rename(mu_hf = X1, sigma_hf = X2) %>% 
   mutate(sigma_hf = sigma_hf * sqrt(240), 
          mu_hf = mu_hf * 240 + 1 / 2 * sigma_hf ^ 2)
@@ -129,66 +158,66 @@ Stock_Daily_Data <- Stock_Daily_Data %>%
 
 
 # Tick数据确定涨跌超过阈值时间点 ----
-# 从Tick文件中匹配第一次超过特定价格的时间的函数
-match_time_up <- function(path, threshold) {
-  # path为tick文件路径
-  # threshold为涨跌整数价格阈值
-  # 返回第一次价格超过阈值的时间
+# 从Tick文件中匹配第一次超过特定价格的时间的函数（并行）
+# 添加需要的变量
+Stock_Daily_Data2 <- Stock_Daily_Data2 %>% 
+  # 生成tick文件路径
+  mutate(tick_path = case_when(
+    substr(S_INFO_WINDCODE, 1L, 3L) %in% c("000", "001") ~ 
+      paste0(path_to_mainboard, "/Tick/SZ/", TRADE_DT, "/", substr(S_INFO_WINDCODE, 1L, 6L), ".csv"), 
+    substr(S_INFO_WINDCODE, 1L, 3L) %in% c("600", "601", "603") ~ 
+      paste0(path_to_mainboard, "/Tick/SH/", TRADE_DT, "/", substr(S_INFO_WINDCODE, 1L, 6L), ".csv")
+  ))
+
+# 将数据集分割成涨跌两部分
+Stock_Daily_Data2_UP <- filter(Stock_Daily_Data2, REACH_UP_THRESHOLD)
+Stock_Daily_Data2_DOWN <- filter(Stock_Daily_Data2, REACH_DOWN_THRESHOLD)
+
+# 生成阈值序列
+Thresholds_UP <- sapply(thresholds, 
+                     function(threshold) 
+                       as.integer(Stock_Daily_Data2_UP$S_DQ_PRECLOSE * (1 + threshold) * 10000))
+Thresholds_DOWN <- sapply(-thresholds, 
+                          function(threshold) 
+                            as.integer(Stock_Daily_Data2_DOWN$S_DQ_PRECLOSE * (1 + threshold) * 10000))
+
+# 从Tick文件中匹配超过阈值的时间点的函数
+# 输入为index，输出为integer vector
+match_time_up <- function(index) {
   tryCatch({
-    path %>% 
+    Stock_Daily_Data2_UP$tick_path[index] %>% 
       read_csv(col_types = cols_only(time = col_integer(), 
                                      high = col_integer())) %>% 
-      with(time[detect_index(high, ~ . >= threshold)])
-  }, error = function(e) NA)
+      with(time[sapply(Thresholds_UP[index, ], function(threshold) detect_index(high, ~ . >= threshold)) %>% replace_certain(0L, NA_integer_)])
+  }, error = function(e) rep(NA_integer_, ncol(Thresholds_UP)))
 }
-
-match_time_down <- function(path, threshold) {
-  # path为tick文件路径
-  # threshold为涨跌整数价格阈值
-  # 返回第一次价格超过阈值的时间
+match_time_down <- function(index) {
   tryCatch({
-    path %>% 
+    Stock_Daily_Data2_DOWN$tick_path[index] %>% 
       read_csv(col_types = cols_only(time = col_integer(), 
                                      low = col_integer())) %>% 
-      with(time[detect_index(low, ~ . > 0 & . <= threshold)])
-  }, error = function(e) NA)
+      with(time[sapply(Thresholds_DOWN[index, ], function(threshold) detect_index(low, ~ . > 0 & . <= threshold)) %>% replace_certain(0L, NA_integer_)])
+  }, error = function(e) rep(NA_integer_, ncol(Thresholds_DOWN)))
 }
 
-# vectorize match_time
-match_time_vector <- function(path, threshold, index, direction = "up") {
-  t1 <- rep.int(NA_integer_, length(threshold))
-  if (direction == "up") {
-    t1[index] <- mcmapply(match_time_up, path[index], threshold[index], USE.NAMES = FALSE, mc.cores = 8L)
-  } else {
-    t1[index] <- mcmapply(match_time_down, path[index], threshold[index], USE.NAMES = FALSE, mc.cores = 8L)
-  }
-  return(t1)
-}
+# start cluster
+cl <- makeCluster(ncl, type = socket_type)
+clusterEvalQ(cl, {
+  library(tidyverse)
+  library(cnquant)
+})
+clusterExport(cl, c(
+  "Stock_Daily_Data2_UP", 
+  "Stock_Daily_Data2_DOWN", 
+  "Thresholds_UP", 
+  "Thresholds_DOWN"
+))
 
-# 添加需要的变量
-Stock_Daily_Data2 <- Stock_Daily_Data %>% 
-  # 生成tick文件路径
-  mutate(tick_path = if_else(
-    substr(S_INFO_WINDCODE, 1L, 1L) == 6, 
-    paste0(path_to_red_ashare, "/Tick/SH/", TRADE_DT, "/", substr(S_INFO_WINDCODE, 1L, 6L), ".csv"), 
-    paste0(path_to_red_ashare, "/Tick/SZ/", TRADE_DT, "/", substr(S_INFO_WINDCODE, 1L, 6L), ".csv")
-  )) %>% 
-  # tick文件中需要用到的整数阈值价格
-  mutate(UP_THRESHOLD_INT = as.integer(UP_THRESHOLD * 10000), 
-         DOWN_THRESHOLD_INT = as.integer(DOWN_THRESHOLD * 10000))
-
-# 计算t1
-system.time(
-Stock_Daily_Data3 <- Stock_Daily_Data2 %>% 
-  mutate(UP_t1 = match_time_vector(tick_path, 
-                                   UP_THRESHOLD_INT, 
-                                   index = S_DQ_HIGH >= UP_THRESHOLD, 
-                                   direction = "up"), 
-         DOWN_t1 = match_time_vector(tick_path, 
-                                     DOWN_THRESHOLD_INT, 
-                                     index = S_DQ_LOW <= DOWN_THRESHOLD, 
-                                     direction = "down"))
-)
+# 主要计算，匹配时间，返回值为integer matrix
+system.time({
+  t1_UP <- parSapply(cl, seq_len(nrow(Stock_Daily_Data2_UP)), match_time_up) %>% t()
+  t1_DOWN <- parSapply(cl, seq_len(nrow(Stock_Daily_Data2_DOWN)), match_time_down) %>% t()
+})
 
 
 # MCMC涨停概率 ----
@@ -201,78 +230,88 @@ ticktime2second <- function(ticktime) {
   return(ticktime)
 }
 
-# 必要数据处理
-Stock_Daily_Data4 <- Stock_Daily_Data3 %>% 
-  select(-tick_path, -UP_THRESHOLD_INT, -DOWN_THRESHOLD_INT) %>% 
-  # 时间变为单位秒
-  mutate_at(vars(UP_t1, DOWN_t1), ticktime2second) %>% 
-  # 计算simulation的N
-  mutate(UP_N = as.integer(round((14400 - UP_t1) / 3)), 
-         DOWN_N = as.integer(round((14400 - DOWN_t1) / 3))) %>% 
-  # 时间变为单位1
-  mutate_at(vars(UP_t1, DOWN_t1), ~ . / 14400) %>% 
-  # 删除开会便到达阈值的点和最后时刻才达到阈值的点
-  filter((REACH_UP_THRESHOLD & between(UP_N, 2L, 4798L)) | 
-         (REACH_DOWN_THRESHOLD & between(DOWN_N, 2L, 4798L)))
+# 时间变为单位秒，并且变为将2个合并为list
+t1 <- list(UP = t1_UP, DOWN = t1_DOWN) %>% 
+  lapply(function(x) apply(x, 2, ticktime2second))
+# 由t1推出T
+T <- t1 %>% 
+  lapply(function(x) (14400L - x) / 14400L)
+# N
+N <- t1 %>% 
+  lapply(function(x) round((14400L - x) / 3))
+# Thresholds_Mod，每个threshold到涨跌停的增长比例
+Thresholds_UP_Mod <- Thresholds_UP %>% 
+  apply(2, function(x) Stock_Daily_Data2_UP$UP_LIMIT * 10000 / x)
+Thresholds_DOWN_Mod <- Thresholds_DOWN %>% 
+  apply(2, function(x) Stock_Daily_Data2_DOWN$DOWN_LIMIT * 10000 / x)
 
 # 计算触及涨停板概率的函数
-prob_reach_up <- function(N, x0, t0, theta, sigma, up_limit) {
+prob_reach_up <- function(index) {
   tryCatch({
-    Sim.DiffProc::GBM(N = N, 
-                      M = 1000, 
-                      x0 = x0, 
-                      t0 = t0, 
-                      theta = theta, 
-                      sigma = sigma) %>% 
-      sapply(max) %>% 
-      {mean(. >= up_limit)}
-  }, error = function(e) NA)
+    trajectories <- Sim.DiffProc::GBM(N = N$UP[index, 1], 
+                                      M = 1000, 
+                                      T = T$UP[index, 1], 
+                                      theta = Stock_Daily_Data2_UP$mu[index], 
+                                      sigma = Stock_Daily_Data2_UP$sigma[index])
+    probs <- rep.int(NA, ncol(T$UP))
+    for (i in seq_along(probs)) {
+      probs[i] <- trajectories[time(trajectories) <= T$UP[index, i], ] %>% 
+        apply(2, max) %>% 
+        {mean(. >= Thresholds_UP_Mod[index, i])}
+    }
+    probs
+  }, error = function(e) rep.int(NA, ncol(T$UP)))
+}
+prob_reach_down <- function(index) {
+  tryCatch({
+    trajectories <- Sim.DiffProc::GBM(N = N$DOWN[index, 1], 
+                                      M = 1000, 
+                                      T = T$DOWN[index, 1], 
+                                      theta = Stock_Daily_Data2_DOWN$mu[index], 
+                                      sigma = Stock_Daily_Data2_DOWN$sigma[index])
+    probs <- rep.int(NA, ncol(T$DOWN))
+    for (i in seq_along(probs)) {
+      probs[i] <- trajectories[time(trajectories) <= T$DOWN[index, i], ] %>% 
+        apply(2, min) %>% 
+        {mean(. <= Thresholds_DOWN_Mod[index, i])}
+    }
+    probs
+  }, error = function(e) rep.int(NA, ncol(T$DOWN)))
 }
 
-prob_reach_down <- function(N, x0, t0, theta, sigma, down_limit) {
-  tryCatch({
-    Sim.DiffProc::GBM(N = N, 
-                      M = 1000, 
-                      x0 = x0, 
-                      t0 = t0, 
-                      theta = theta, 
-                      sigma = sigma) %>% 
-      sapply(min) %>% 
-      {mean(. <= down_limit)}
-  }, error = function(e) NA)
-}
+# clusterExport
+clusterExport(cl, c(
+  "N", 
+  "T", 
+  "Thresholds_UP_Mod", 
+  "Thresholds_DOWN_Mod"
+))
 
-# 计算触及涨停的概率
-system.time(
-  Stock_Daily_Data4_UP <- Stock_Daily_Data4 %>% 
-    filter(REACH_UP_THRESHOLD & between(UP_N, 2L, 4798L)) %>% 
-    mutate(PROB_REACH_UP_LIMIT = mcmapply(prob_reach_up, UP_N, UP_THRESHOLD, UP_t1, mu, sigma, UP_LIMIT, USE.NAMES = FALSE), 
-           PROB_REACH_UP_LIMIT_hf = mcmapply(prob_reach_up, UP_N, UP_THRESHOLD, UP_t1, mu_hf, sigma_hf, UP_LIMIT, USE.NAMES = FALSE))
-)
+set.seed(4L)
 
-# 计算触及跌停的概率
-system.time(
-  Stock_Daily_Data4_DOWN <- Stock_Daily_Data4 %>% 
-    filter(REACH_DOWN_THRESHOLD & between(DOWN_N, 2L, 4798L)) %>% 
-    mutate(PROB_REACH_DOWN_LIMIT = mcmapply(prob_reach_down, DOWN_N, DOWN_THRESHOLD, DOWN_t1, mu, sigma, DOWN_LIMIT, USE.NAMES = FALSE), 
-           PROB_REACH_DOWN_LIMIT_hf = mcmapply(prob_reach_down, DOWN_N, DOWN_THRESHOLD, DOWN_t1, mu_hf, sigma_hf, DOWN_LIMIT, USE.NAMES = FALSE))
-)
+# 主要计算，达到各个threshold后达到涨跌停的概率，返回值为numeric matrix
+system.time({
+  Probs_UP <- parSapply(cl, seq_len(nrow(Stock_Daily_Data2_UP)), prob_reach_up) %>% t()
+  Probs_DOWN <- parSapply(cl, seq_len(nrow(Stock_Daily_Data2_DOWN)), prob_reach_down) %>% t()
+})
 
-save.image("data/01-D-First-Complete-Data.RData")
+# 结束并行cluster
+stopCluster(cl)
+
+save.image("data/01-D-Sequential-Data.RData")
 
 
 # 描述性统计 ----
 # 日最大收益率的分布
-Stock_Daily_Data0 %>% 
+Stock_Daily_Data1 %>% 
   mutate(HIGH_PCTCHANGE = S_DQ_HIGH / S_DQ_PRECLOSE - 1) %>% 
   ggplot(aes(x = HIGH_PCTCHANGE)) + 
   geom_histogram(binwidth = 0.0001)
 
 
 # 理论概率与真实概率 ----
-Stock_Daily_Data4_UP %>% 
+Stock_Daily_Data2_UP %>% 
   summarise(REACH_UP_LIMIT_mean = mean(REACH_UP_LIMIT), 
-            PROB_REACH_UP_LIMIT_mean = mean(PROB_REACH_UP_LIMIT, na.rm = TRUE), 
+            PROB_REACH_UP_LIMIT_mean = mean(Probs_UP[, 1], na.rm = TRUE), 
             REACH_UP_diff = REACH_UP_LIMIT_mean - PROB_REACH_UP_LIMIT_mean, 
-            REACH_UP_p.value = t.test(REACH_UP_LIMIT, PROB_REACH_UP_LIMIT)$p.value)
-
+            REACH_UP_p.value = t.test(REACH_UP_LIMIT, Probs_UP[, 1], paired = TRUE)$p.value)
